f4886843f1dbcb7188e6ea05bb302e2c368663b5
Alex Silva <damasioesilva@gmail.com>
2021-08-23 22:19:14 -0300

    layout e funcao calcular

---
+++ app/src/main/java/com/example/calculavalordeliveryjava/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.calculavalordeliveryjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView valorProduto;
+    private TextView porcentagemDelivery;
+    private TextView valorIdeal;
+    private Button calcular;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        valorProduto = findViewById(R.id.text_valor_produto);
+        porcentagemDelivery = findViewById(R.id.text_porcrentagem_delivery);
+        valorIdeal = findViewById(R.id.text_valor_ideal);
+        calcular = findViewById(R.id.button_calcular);
+
+        calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calcula();
+            }
+        });
+
+    }
+
+    public void calcula(){
+
+        Double valor = Double.valueOf(valorProduto.getText().toString());
+        Double porcentagem = Double.valueOf(porcentagemDelivery.getText().toString());;
+        Double resultado;
+
+
+        resultado = valor / (1 - ( porcentagem/100));
+        valorIdeal.setText(resultado.toString());
+
+    }
+}
